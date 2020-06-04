05fa87b6b6a9bfd585e05351ee41e60e7f6198bd
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-06-04 21:33:55 +0200

    feat: Add basic routes

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
+
 const app = express();
 const PORT = 5000;
+
+// Index route
+app.get('/', (req, res) => {
+  res.send('INDEX');
+});
+
+app.get('/about', (req, res) => {
+  res.send('ABOUT');
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
