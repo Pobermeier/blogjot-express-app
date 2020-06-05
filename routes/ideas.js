30cd4990681eb2f790053afc9c373342fe1e4109
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-06-05 20:16:17 +0200

    feat: Add access control for per-user content

--- routes/ideas.js
+++ routes/ideas.js
@@ -9,7 +9,7 @@ const Idea = mongoose.model('ideas');
 
 // Idea Index Page
 router.get('/', ensureAuthenticated, (req, res) => {
-  Idea.find({})
+  Idea.find({ user: req.user.id })
     .sort({ date: 'desc' })
     .then((ideas) => {
       res.render('ideas/index', {
@@ -27,7 +27,14 @@ router.get('/add', ensureAuthenticated, (req, res) => {
 router.get('/edit/:id', ensureAuthenticated, (req, res) => {
   Idea.findOne({
     _id: req.params.id,
-  }).then((idea) => res.render('ideas/edit', { idea }));
+  }).then((idea) => {
+    if (idea.user != req.user.id) {
+      req.flash('error_msg', 'Not authorized');
+      res.redirect('/ideas');
+    } else {
+      res.render('ideas/edit', { idea });
+    }
+  });
 });
 
 // Process "Add Idea"-Form requests
@@ -51,6 +58,7 @@ router.post('/', ensureAuthenticated, (req, res) => {
     const newUser = {
       title: req.body.title,
       details: req.body.details,
+      user: req.user.id,
     };
     new Idea(newUser).save().then((idea) => {
       req.flash('success_msg', 'Idea added');
