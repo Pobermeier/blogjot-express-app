ad427a9b81c6a94b4c91be2ad240580cf9618570
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-06-05 11:02:55 +0200

    feat: Add functionality to save blog-idea to db

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 5000;
 
-// Connect to Mongoose
+// Connect to DB via Mongoose
 mongoose
   .connect(
     'mongodb+srv://pober:BuA7yjysl6bEsU9d@cluster0-uclm9.mongodb.net/blogjot?retryWrites=true&w=majority',
@@ -25,6 +26,10 @@ const Idea = mongoose.model('ideas');
 // Handlebars Middleware
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+// Body-parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Index route
 app.get('/', (req, res) => {
@@ -46,7 +51,30 @@ app.get('/ideas/add', (req, res) => {
 
 // Process "Add Idea"-Form requests
 app.post('/ideas', (req, res) => {
-  res.send('ok');
+  let errors = [];
+
+  if (!req.body.title) {
+    errors.push({ text: 'Please add a title' });
+  }
+  if (!req.body.details) {
+    errors.push({ text: 'Please add some details text' });
+  }
+
+  if (errors.length > 0) {
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details,
+    });
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details,
+    };
+    new Idea(newUser).save().then((idea) => {
+      res.redirect('/ideas');
+    });
+  }
 });
 
 app.listen(PORT, () => {
