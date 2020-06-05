cbfa0fc61e4557ff2eb484b0b2ef02cfd42ecaaa
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-06-05 18:13:59 +0200

    feat: Implemented Logout-functionality

--- routes/users.js
+++ routes/users.js
@@ -85,4 +85,11 @@ router.post('/register', (req, res) => {
   }
 });
 
+// Logout user
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are now logged out.');
+  res.redirect('/users/login');
+});
+
 module.exports = router;
