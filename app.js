6a1951ab37a156335a3ea6d19ac0d3778eadaf89
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-06-05 10:08:27 +0200

    feat: Add mongoose to project deps and initialize it

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 5000;
 
-// Handlebars
+// Connect to Mongoose
+mongoose
+  .connect(
+    'mongodb+srv://pober:BuA7yjysl6bEsU9d@cluster0-uclm9.mongodb.net/blogjot?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+  )
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Handlebars Middleware
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
